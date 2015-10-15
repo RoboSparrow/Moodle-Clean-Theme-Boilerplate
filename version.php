679985977b2ae5f6c23cafa0e43de1c93af483a6
Joerg Boeselt <joerg@brightcookie.com.au>
2015-10-15 17:16:51 +1030

    version, phpdoc

--- version.php
+++ version.php
@@ -23,25 +23,17 @@
  * For full information about creating Moodle themes, see:
  * http://docs.moodle.org/dev/Themes_2.0
  *
- * @package   theme_cleanchild
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    theme_cleanchild
+ * @see https://docs.moodle.org/dev/version.php
  */
 
-$THEME->name = 'cleanchild';
+defined('MOODLE_INTERNAL') || die;
 
-/////////////////////////////////
-// The only thing you need to change in this file when copying it to
-// create a new theme is the name above. You also need to change the name
-// in version.php and lang/en/theme_clean.php as well.
-//////////////////////////////////
-//
-$THEME->doctype = 'html5';
-$THEME->parents = array('bootstrapbase', 'clean');
-$THEME->sheets = array('custom','cleanchild');
-$THEME->supportscssoptimisation = false;
-$THEME->yuicssmodules = array();
-$THEME->enable_dock = true;
-$THEME->editor_sheets = array();
+$plugin->version    = 2015100100; // theme version
+$plugin->requires   = 2015050500; /// requires: oodle version
+$plugin->component  = 'theme_cleanchild';
 
-$THEME->rendererfactory = 'theme_overridden_renderer_factory';
-$THEME->csspostprocess = 'theme_clean_process_css';
+$plugin->dependencies = array(
+    'theme_bootstrapbase'   => 2015050500,
+    'theme_clean'           => 2015051100,
+);
